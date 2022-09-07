b07818c770ec9296a8203b950e69c81bdb373769
Andrianns <andrianaji18@yahoo.com>
2022-09-07 23:00:13 +0700

    test request sent invatitaion from nodemailer

--- server/services/app/routes/invitationRouter.js
+++ server/services/app/routes/invitationRouter.js
@@ -1,2 +1,6 @@
-const router = require("express").Router();
+const InvitationController = require('../controllers/invitationController');
+
+const router = require('express').Router();
+
+router.get('/sendMail/:id', InvitationController.sendMailInvitation);
 module.exports = router;

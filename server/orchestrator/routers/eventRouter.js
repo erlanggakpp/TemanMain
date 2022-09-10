e1837e94a0178f60f366d874326cc40276589793
erlanggakpp <erlanggakencana@gmail.com>
2022-09-10 18:34:51 +0700

    Bug fixes

--- server/orchestrator/routers/eventRouter.js
+++ server/orchestrator/routers/eventRouter.js
@@ -2,8 +2,12 @@ const EventController = require("../controllers/eventController");
 const authenticator = require("../middlewares/authenticator");
 
 const router = require("express").Router();
-
+router.use(authenticator);
 router.get("/", EventController.getAllEvents);
-router.get("/:eventId", authenticator, EventController.findOneEvent);
+router.post("/", EventController.createEvent);
+router.get("/:eventId", EventController.findOneEvent);
+router.put("/:eventId", EventController.editEvent);
+router.delete("/:eventId", EventController.deleteEvent);
+router.get("/category/:categoryId", EventController.getEventsbyCategory);
 
 module.exports = router;

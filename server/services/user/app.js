40da0962fde3f148129ca2e9804f5de8c7a2a365
erlanggakpp <erlanggakencana@gmail.com>
2022-09-11 00:45:16 +0700

    Feat: Fix seed bugs

--- server/services/user/app.js
+++ server/services/user/app.js
@@ -1,9 +1,11 @@
-const express = require('express')
-const app = express()
-const router = require('./routers/index.js')
+const express = require("express");
+const app = express();
+const router = require("./routers/index.js");
+const cors = require("cors");
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-app.use(router)
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(router);
 
-module.exports = app
+module.exports = app;

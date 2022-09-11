7db8f378fe7e965fe14bde4b154030fe106cd5da
erlanggakpp <erlanggakencana@gmail.com>
2022-09-11 20:54:50 +0700

    Feat: added new route for fetch one category

--- server/orchestrator/routers/categoryRouter.js
+++ server/orchestrator/routers/categoryRouter.js
@@ -1,9 +1,10 @@
-const categoryRouter = require('express').Router()
-const CategoryController = require('../controllers/CategoryController.js')
+const categoryRouter = require("express").Router();
+const CategoryController = require("../controllers/CategoryController.js");
 
-categoryRouter.get('/', CategoryController.readAllCategory)
-categoryRouter.post('/', CategoryController.createCategory)
+categoryRouter.get("/", CategoryController.readAllCategory);
+categoryRouter.post("/", CategoryController.createCategory);
 categoryRouter.put("/:categoryId", CategoryController.updateCategory);
+categoryRouter.get("/:categoryId", CategoryController.fetchOneCategory);
 categoryRouter.delete("/:categoryId", CategoryController.deleteCategory);
 
-module.exports = categoryRouter
+module.exports = categoryRouter;

ef6bb355f3ecf05a87bec05c5e9fd98c1a72e654
salwazahramunir <salwazahramunir@gmail.com>
2022-09-14 00:04:49 +0700

    agora sandbox

---
+++ agora/seriusan-nih/src/config/index.js
@@ -0,0 +1,28 @@
+export const rtc = {
+  // For the local client.
+  client: null,
+  // For the local audio and video tracks.
+  videoTrack: null,
+  audioTrack: null
+};
+
+export const options = {
+  // Pass your app ID here.
+  appId: "ad0131b9c21f4d9bbf79540050f11f88",
+  // Set the channel name.
+  channel: "Test",
+    uid: null,
+
+  // Pass a token if your project enables the App Certificate.
+  token: "007eJxTYLjzyuaB4ZV2/3zVFQKu5q2xjTP6647qPW+b2/4n/aXow0kKDIkpBobGhkmWyUaGaSYplklJaeaWpiYGBqYGaYaGaRYWZ67IJXupKSSXcwYwMTJAIIjPwhCSWlzCwAAAoNAgLw==",
+};
+
+
+export const options2 = {
+  // Pass your app ID here.
+  appId: "02cc5abf5b1142a693e7d9be39fb8991",
+  // Set the channel name.
+  channel: "TemanMain",
+  // Pass a token if your project enables the App Certificate.
+  token: "007eJxTYIioirwW+erf6S98FhJWWzxCorfN4BX+VtQVf6h0CZPltD4FBgOj5GTTxKQ00yRDQxOjRDNL41TzFMukVGPLtCQLS0tD+clyyfOeyCcLW19iZGSAQBCfkyEkNTcxzzcxM4+BAQD7bCJY",
+};
